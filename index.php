523046571c416a8a0f0c3a852f8e7c1f845ee729
fatemasaifee8 <fatemasaifee8@gmail.com>
2017-03-09 22:57:22 +0530

    Initial Design of Index page

---
+++ index.php
@@ -0,0 +1,442 @@
+<!-- Bismillah -->
+
+<!DOCTYPE html>
+<html lan="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>Badshah Mandap| Decorating Your Moments </title>
+	<meta name="description" content="Badshah Mandap">
+
+	<!-- Bootstrap CDN -->
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="custom.css"></link>
+
+	
+</head>
+<body style="background-color: #EAECEE">
+
+<!-- Nav Bar --> 
+<nav class="navbar navbar-custom navbar-fixed-top" id="my-navbar">
+	<div class="container">
+	<div class="navbar-header">
+	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+	<span class="icon-bar"></span>
+	<span class="icon-bar"></span>
+	<span class="icon-bar"></span>
+	<span class="icon-bar"></span>
+	</button>
+	<a href="index.php" class="navbar-brand">Badshah Mandap</a>
+	</div> <!--End Navbar Header -->
+
+	<div class="collapse navbar-collapse" id="navbar-collapse">
+	<ul class="nav navbar-nav">
+		<li><a href="#AboutUs">About Us</a></li>
+		<li><a href="#Gallery">Gallery</a></li>
+		<li><a href="#Services">Services</a></li>
+		<li><a href="#Testimonial">Testimonial</a></li>
+		<li><a href="#ContactUs">Contact Us</a></li>
+	</ul>
+	</div>
+	</div><!-- End Container --> 
+
+</nav>
+
+<!-- jumbotron-->
+
+  	<div class="jumbotron" style="background-color:#9F000F">
+  		<div class="container text-center" style="color: white">
+  		<br>
+  		<br>
+  			<img src="badshah.jpg" class="img-circle">
+  			<h2>Badshah Mandap Decorators</h2>
+  			<p>Decorating Your Experience</p>
+
+  		</div><!-- End container -->
+  	</div><!-- End jumbotron-->
+
+
+<!-- Nav Bar Ends -->
+
+<!-- About Us Section -->
+
+<section>
+	<div class="page-holder" id="AboutUs">
+	<div class="container" >
+		<h2>About Us</h2>
+		<div class="divider-header"></div>
+		<p>
+		Badshah Mandap Decorators. We provide Decoration services for Memorable Occasions like Wedding, Reception, Betrothal, Birthday Parties, House warming, Religious Ceremony, Social get-together, Corporate Events, Conferences, Meetings, Family Gatherings, Anniversaries, Stage Shows, Exhibition and so on.
+		</p>
+		<p>
+		Badshah Mandap is to make your dream celebration a reality. We would provide Best of our Services and Make your celebrations special and Memorable. 
+		</p>
+    <p>
+    Badshah Mnadap Decorators is a venture in the profession of mandapam decoration services for more than twenty years. We take decoration contracts for various occasions – be it a Marriage Function or a Marriage Reception, a Religious Ceremony or Function, a Social get-together or a Corporate Meeting, a Stage Show or Performance – we assure to make your special occasion, the most memorable. 
+    We specialize in Wedding Decoration in general and Indian wedding decoration is our forte. Our team of designers and wedding decorators create that perfect look – be it theme weddings, reception, flower arrangements, stage backdrops, varmala setup, gate decoration, food canopies’, etc. 
+    </p>
+        	
+        		
+		</div>
+	</div>
+	</section>
+<!-- /Section: about -->
+
+<!-- Carousal Gallery Section -->
+<!-- Gallery -->
+	<div class="container">
+		<section>
+			<div class="page-header" id="Gallery">
+  				<h2>Gallery.<small> Have A Glimps..</small></h2>
+  			</div>
+
+  			<div class="carousel slide" id="screenshot-carousel" data-ride="carousel">
+  				<ol class="carousel-indicators">
+  					<li data-target="#screenshot-carousel" data-slide-to="0" class="active"></li>
+  					<li data-target="#screenshot-carousel" data-slide-to="1"></li>
+  					<li data-target="#screenshot-carousel" data-slide-to="2"></li>
+  					<li data-target="#screenshot-carousel" data-slide-to="3"></li>
+  				</ol>
+  				<div class="carousel-inner">
+  					<div class="item active">
+  						<img src="22.jpg" alt="Text of the image">
+  						<div class="carousel-caption">
+  							<h3>Wedding Stage</h3>
+  							<p>Setup for varmala Ceremony</p>
+  						</div>
+  					</div>
+  					<div class="item">
+  						<img src="39.jpg" alt="Text of the image">
+  						<div class="carousel-caption">
+  							<h3>Shows Setup</h3>
+  							<p>Stage for Shows and Events</p>
+  						</div>
+  					</div>
+  					<div class="item">
+  						<img src="51.jpg" alt="Text of the image">
+  						<div class="carousel-caption">
+  							<h3>Corporate Event</h3>
+  							<p>Setup for Corporate Events</p>
+  						</div>
+  					</div>
+  					<div class="item">
+  						<img src="bg-2.jpg" alt="Text of the image">
+  						<div class="carousel-caption">
+  							<h3>Ladies Sangeet</h3>
+  							<p>Stage Setup for Ladies Sangeet Ceremony</p>
+  						</div>
+  					</div>
+
+  				</div><!-- End Carousel inner -->
+  				<a href="#screenshot-carousel" class="left carousel-control" data-slide="prev">
+  					<span class="glyphicon glyphicon-chevron-left"></span>
+  				</a>
+  				<a href="#screenshot-carousel" class="right carousel-control" data-slide="next">
+  					<span class="glyphicon glyphicon-chevron-right"></span>
+  				</a>
+  			</div><!-- End Carousel -->
+
+		</section>
+	</div>
+
+<!-- Carousal Gallery Section End -->
+
+<!-- Services Section -->
+<div class="container">
+<section>
+	<div class="page-holder" id="Services">
+	<h2> Services <small>....</small> </h2>
+	</div>
+	<!-- <div class="row">
+		<div class="col-lg-8">
+		<h3> Mahila Sangeet </h3>
+		<p>...</p>
+		</div> 
+
+		<div class="col-lg-4">
+		<img src="" class="img-responsive" alt="Service">
+		</div>	
+	</div> End row -->
+
+	<hr>
+
+	<div class="row">
+		<div class="col-lg-4">
+		<div class="panel panel-default text-center">
+		<div class="panel-body">
+		<h4>Starnigts Setups</h4>
+		<p>.....</p>
+		</div>
+		</div>
+		</div> <!-- End Column -->
+
+		<div class="col-lg-4">
+		<div class="panel panel-default text-center">
+		<div class="panel-body">
+		<h4>Fashion Shows Setups</h4>
+		<p>.....</p>
+		</div>
+		</div>
+		</div> <!-- End Column -->
+
+		<div class="col-lg-4">
+		<div class="panel panel-default text-center">
+		<div class="panel-body">
+		<h4>Product Launch Setups</h4>
+		<p>.....</p>
+		</div>
+		</div>
+		</div> <!-- End Column -->
+	</div> <!-- End Row -->
+
+	<div class="row">
+		<div class="col-lg-4">
+		<div class="panel panel-default text-center">
+		<div class="panel-body">
+		<h4>Theme Parties</h4>
+		<p>.....</p>
+		</div>
+		</div>
+		</div> <!-- End Column -->
+
+		<div class="col-lg-4">
+		<div class="panel panel-default text-center">
+		<div class="panel-body">
+		<h4>Wedding</h4>
+		<p>.....</p>
+		</div>
+		</div>
+		</div> <!-- End Column -->
+
+		<div class="col-lg-4">
+		<div class="panel panel-default text-center">
+		<div class="panel-body">
+		<h4>Birthday Stage</h4>
+		<p>.....</p>
+		</div>
+		</div>
+		</div> <!-- End Column -->
+	</div> <!-- End Row -->
+
+	<div class="row">
+		<div class="col-lg-4">
+		<div class="panel panel-default text-center">
+		<div class="panel-body">
+		<h4>Varmala Setups</h4>
+		<p>.....</p>
+		</div>
+		</div>
+		</div> <!-- End Column -->
+
+		<div class="col-lg-4">
+		<div class="panel panel-default text-center">
+		<div class="panel-body">
+		<h4>Ladies Sangeet Setups</h4>
+		<p>.....</p>
+		</div>
+		</div>
+		</div> <!-- End Column -->
+
+		<div class="col-lg-4">
+		<div class="panel panel-default text-center">
+		<div class="panel-body">
+		<h4>Corporate Events</h4>
+		<p>.....</p>
+		</div>
+		</div>
+		</div> <!-- End Column -->
+	</div> <!-- End Row -->
+
+
+
+
+</section>
+</div>
+<!-- Services Section Ends -->
+
+<!-- Testimonials-->
+  	<div class="container">
+  		<section>
+  			<div class="page-header" id="feedback">
+  				<h2>Testimonial.<small> Listen from our customers</small></h2>
+  			</div>
+
+  			<div class="row">
+  				<div class="col-md-4">
+  					<blockquote>
+  						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi gravida quam ac ante rutrum, in mollis ligula mattis. Integer nulla nisi, ullamcorper et posuere vel</p>
+  						<footer>John doe</footer>
+  					</blockquote>
+  				</div>
+  				<div class="col-md-4">
+  					<blockquote>
+  						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi gravida quam ac ante rutrum, in mollis ligula mattis. Integer nulla nisi, ullamcorper et posuere vel</p>
+  						<footer>John doe</footer>
+  					</blockquote>
+  				</div>
+  				<div class="col-md-4">
+  					<blockquote>
+  						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi gravida quam ac ante rutrum, in mollis ligula mattis. Integer nulla nisi, ullamcorper et posuere vel</p>
+  						<footer>John doe</footer>
+  					</blockquote>
+  				</div>
+  			</div><!-- End row -->
+  		</section>
+  	</div><!--End Container-->
+
+<!-- Testimonial Ends -->
+
+
+<!-- Query Section -->
+
+    <div class="container">
+      <section>
+        <div class="page-header" id="faq">
+          <h2>FAQ.<small> Engaging with consumers.</small></h2>
+        </div><!-- End Page Header -->
+
+        <div class="panel-group" id="accordion">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <div class="panel-title">
+                <a href="#collapse-1" data-toggle="collapse" data-parent="#accordion">
+                  Question one?
+                </a>
+              </div><!-- End panel title -->
+              <div id="collapse-1" class="panel-collapse collapse in">
+                <div class="panel-body">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas non urna in fringilla. Praesent consequat est at feugiat faucibus
+                </div>
+              </div><!-- End Panel collapse -->
+            </div>
+          </div>
+
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <div class="panel-title">
+                <a href="#collapse-2" data-toggle="collapse" data-parent="#accordion">
+                  Question Two?
+                </a>
+              </div><!-- End panel title -->
+              <div id="collapse-2" class="panel-collapse collapse">
+                <div class="panel-body">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas non urna in fringilla. Praesent consequat est at feugiat faucibus
+                </div>
+              </div><!-- End Panel collapse -->
+            </div>
+          </div>
+
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <div class="panel-title">
+                <a href="#collapse-3" data-toggle="collapse" data-parent="#accordion">
+                  Question Three?
+                </a>
+              </div><!-- End panel title -->
+              <div id="collapse-3" class="panel-collapse collapse">
+                <div class="panel-body">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas non urna in fringilla. Praesent consequat est at feugiat faucibus
+                </div>
+              </div><!-- End Panel collapse -->
+            </div>
+          </div>
+        </div><!-- End panel group -->
+
+      </section>
+    </div><!-- End container -->
+
+<!-- Query Section Ends -->
+<!-- Contact -->
+
+  <div class="container">
+    <section>
+      <div class="page-header" id="contact">
+          <h2>Contact Us.<small> Contact us about Services we provide.</small></h2>
+        </div><!-- End Page Header -->
+
+        <div class="row">
+          <div class="col-lg-4">
+            <p>Send us a message, or contact us from the address below</p>
+
+
+            <address>
+              <strong>Badshah Mandap Decorators</strong></br>
+              76,Ahliya Pura</br>
+              Indore-452002</br>
+              P: +91 9826056665
+            </address>
+          </div>
+          
+          <div class="col-lg-8">
+            <form action="" class="form-horizontal">
+              <div class="form-group">
+                <label for="user-name" class="col-lg-2 control-label">Name</label>
+                <div class="col-lg-10">
+                  <input type="text" class="form-control" id="user-name" placeholder="Enter you name">
+                </div>
+              </div><!-- End form group -->
+
+              <div class="form-group">
+                <label for="user-email" class="col-lg-2 control-label">Email</label>
+                <div class="col-lg-10">
+                  <input type="text" class="form-control" id="user-email" placeholder="Enter you Email Address">
+                </div>
+              </div><!-- End form group -->
+
+              <div class="form-group">
+                <label for="user-message" class="col-lg-2 control-label">Any Message</label>
+                <div class="col-lg-10">
+                  <textarea name="user-message" id="user-message" class="form-control" 
+                  cols="20" rows="10" placeholder="Enter your Message"></textarea>
+                </div>
+              </div><!-- End form group -->
+
+              <div class="form-group">
+                <div class="col-lg-10 col-lg-offset-2">
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+              </div>
+
+             
+
+            </form>
+          </div>
+        </div><!-- End the row -->
+
+    </section>
+  </div>
+
+  <!--Contact Section Ends -->
+
+<!-- Footer -->
+
+    <footer>
+      <hr>
+        <div class="container text-center">
+       
+       
+        <hr>
+        <ul class="list-inline">
+          <li><a href="http://www.twitter.com/wiredwiki">Twitter</a></li>
+          <li><a href="http://www.facebook.com/askorama">Facebook</a></li>
+          <li><a href="http://www.youtube.com/wiredwiki">YouTube</a></li>
+        </ul>
+
+        <p>&copy; Copyright @ 2017</p>
+
+      </div><!-- end Container-->
+      
+
+    </footer>
+
+
+<script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script type="text/javascript" src="custom.js"></script>
+
+</body>
+</html>
